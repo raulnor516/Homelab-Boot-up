f37ce8667c50d5752da448929e517644d68076da
raulnor516 <hyde.logan@gmail.com>
2017-10-10 14:03:52 -0500

    Fixed the error handling to be more verbose

--- siege_launch.bat
+++ siege_launch.bat
@@ -11,18 +11,18 @@ fi
 while getopts ':u:p:' CRED
 do
     case $CRED in
-        u) uflag=1
+        u)  uflag=1
             username="$OPTARG"
             ;;
-        p) pflag=1
+        p)  pflag=1
             password="$OPTARG"
             ;;
-        /?) printf "Bad switch given. Use [-u username] and [-p password]. \n" $(basename $0) >&2
+        /?) printf "&OPTARG isn't a viable option. Use [-u username] and [-p password]. \n" $(basename $0) >&2
         #So that last line just gives some error handling if bad switches are given.
         #The basename $0 thing is supposed to return the name of the script that errored, to stderror
             exit 2
             ;;
-        /:) printf "You need to supply credentials after each -u or -p. \n" >&2
+        /:) printf "You need to supply credentials after &OPTARG. \n" $(basename $0) >&2
         # If someone provides a -u or -p without text after it, this will tell them to add info
             exit 2
             ;;
